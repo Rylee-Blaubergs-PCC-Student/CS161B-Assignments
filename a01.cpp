8a0ada2f2a18990591fc8d5fe407d290b09e5e9e
Rylee-Blaubergs-PCC-Student <rylee.blaubergs@pcc.edu>
2026-01-13 23:16:49 -0800

    UPdate

--- a01.cpp
+++ a01.cpp
@@ -2,10 +2,10 @@
 // Author:           Rylee Blaubergs
 // Assignment:       Assignment 1
 // Date:             1/13/26
-// Description:      
-// Input:            
-// Output:           
-// Sources:          
+// Description:      Make a food cart shopping program
+// Input:            dish name as string, price amount as double, 
+// Output:           recipt of all items
+// Sources:          Assignment instructions and Sample Code
 //                   
 //                   
 //******************************************************************************
@@ -14,6 +14,56 @@ using namespace std;
 
 // function prototypes
 void welcome();
+void displayMenu();
+void readOption(int &option); // name of order
+void placeOrder(double &cost); // cost of order
+void readInt(string prompt, int &num)
+void readDouble(string prompt, double &num);
+void calcTotal();
+double tipDiscount(double &tip, double &discount, double cost)
+void calcFinalTotal();
+void goodbye();
+
+int main() {
 
 
 
+
+//welcome message
+
+//diplay menu
+
+
+
+
+
+
+
+
+}
+
+void welcome() {
+    cout << "Welcome to my Food Cart Program!" << endl;
+}
+
+void displayMenu() {
+    cout << "What would you like to do?" << endl;
+    cout << setw(8) << right << "1. Place an order" << endl;
+    cout << setw(8) << right << "2. Quit" << endl;
+}
+
+int readInt(string prompt, int &num)
+{
+  int tempVar = 0;
+  cout << prompt;
+  cin >> tempVar;
+  while(!cin)
+  {
+    cout << "Invalid input! Please try again!!" << endl;
+    cin.clear();
+    cin.ignore(100, '\n');
+    cin >> tempVar;
+  }
+  cin.ignore(10, '\n');
+  return tempVar;
+}
