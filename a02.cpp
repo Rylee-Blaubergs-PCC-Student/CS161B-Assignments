9d31fa3d91ade0c7ead063606e3159f2ef8ae65d
Rylee-Blaubergs-PCC-Student <rylee.blaubergs@pcc.edu>
2026-01-19 17:50:57 -0800

    Add a02.cpp
    Initial commit

---
+++ a02.cpp
@@ -0,0 +1,11 @@
+//******************************************************************************
+// Author:           Rylee Blaubergs
+// Assignment:       Assignment 1
+// Date:             1/13/26
+// Description:      Make a food cart shopping program
+// Input:            dish name as string, price amount as double, 
+// Output:           recipt of all items
+// Sources:          Assignment instructions, Sample Code, previous term code(for the string validation) 
+//                   
+//                   
+//******************************************************************************
