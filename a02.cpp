cb59169fe5a4ee63457a4404e640fa1dba2d4852
Rylee-Blaubergs-PCC-Student <rylee.blaubergs@pcc.edu>
2026-01-24 18:32:04 -0800

    update

--- a02.cpp
+++ a02.cpp
@@ -12,7 +12,7 @@
 //******************************************************************************
 #include <iostream>
 #include <iomanip>
-#include <string>
+#include <cstring>
 using namespace std;
 
 void welcome();
