d92d5e76c438dfac74911b65d6f2b1ebec8c2695
Rylee-Blaubergs-PCC-Student <rylee.blaubergs@pcc.edu>
2026-02-01 04:07:09 -0800

    Add header comments to a03.cpp
    Added header comments with author and assignment details.

---
+++ a03.cpp
@@ -0,0 +1,12 @@
+//******************************************************************************
+// Author:           Rylee Blaubergs
+// Assignment:       Assignment 3
+// Date:             2/1/26
+// Description:      
+// Input:            
+//                   
+// Output:           
+// Sources:          Assignment instructions, Sample Code, Previous assignments
+//                   
+//                   
+//******************************************************************************
