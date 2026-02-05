88b411d13ad905d248124af7759b63f6f317059a
Rylee-Blaubergs-PCC-Student <rylee.blaubergs@pcc.edu>
2026-02-04 18:00:58 -0800

    Create midterm.cpp

---
+++ midterm.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+using namespace std;
+
+//constants and function prototypes
+void printList(const int[], int size);
+
+
+int main() {
+
+
+
+
+	return 0;
+}
+
+
+
+void printList(const int list[], int size)
+{
+	for (int i = 0; i < size; i++)
+	{
+		cout << list[i] << endl;
+	}
+}
