40aa6cdf52e526d30ec8edf85227cbaee475c334
Rylee-Blaubergs-PCC-Student <rylee.blaubergs@pcc.edu>
2026-02-04 19:09:12 -0800

    update

--- midterm.cpp
+++ midterm.cpp
@@ -4,7 +4,8 @@ using namespace std;
 
 // function prototypes:
 void welcome();
-void readOption();
+void goodbye();
+void readOption(char &option);
 int enqueue(int queue[], int &size, int val);
 int dequeue(int queue[], int &size, int val);
 void printQueue(int queue[], int size);
@@ -18,29 +19,44 @@ int main() {
 
     welcome();
     
-
-    while (option != 'q') {
-     
-     // check if we should run enqueue or dequeue
-     if (option == '+') {
-         
-         
-     } else if (option == '-') {
-     
-     
-     }
-     // this happens if neither are entered
-     cout << "Invalid Option." << endl << endl;
-     readOption();
-        
-    }
-
+    readOption(option);
     
-    return 0;
+    
+     while (option == '+' || option == '-') {
+     // check if we should run enqueue, dequeue, or to quit
+     if (option == '+') {
+         cout << "+ was entered" << endl;
+         readOption(option);
+         
+     } 
+     if (option == '-') {
+         cout << "- was entered" << endl;
+         readOption(option);
+
+     }
+     if (option == 'q') {
+         goodbye();
+         return 0;
+     }
+     
+     // this happens nothing above have been entered
+     cout << "Invalid Option." << endl << endl;
+     readOption(option);
+     }
+
 }
 
 void welcome() {
     cout << "Welcome to the FIFO Queue Program!" << endl << endl;
+}
+
+void goodbye() {
+    cout << endl << "Goodbye" << endl;
+}
+
+void readOption(char &option) {
+    cout << "Enter Option: ";
+    cin >> option; 
 }
 
 // Returns 0 on success, 1 on overflow (queue is full, cannot add)
@@ -50,6 +66,7 @@ int enqueue(int queue[], int &size, int val){
 // add your code here
     
 }
+
 
 // Returns 0 on success, 1 on underflow (queue is empty, no values to dequeue), 2 if integer is not in the queue.
 
